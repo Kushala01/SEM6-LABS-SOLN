bfc507c3d2fed931646875d018c0c309bfda95b9
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2024-01-17 16:10:18 +0530

    C

--- PPL/LAB3/1.c
+++ PPL/LAB3/1.c
@@ -2,39 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
-void main (int argc, char * argv[]) {
+#define MCW MPI_COMM_WORLD
+#define ll long long
+
+int main(int argc, char *argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    long fact;
-    long i, n;
-    long rec;
-    long arr[100], facts[100];
+    MPI_Comm_rank(MCW, &rank);
+    MPI_Comm_size(MCW, &size);
+    ll int fact;
+    ll int i, n;
+    ll int rec;
+    ll int arr[100], facts[100];
+
     if (rank == 0) {
         n = size;
         printf("Enter the numbers: ");
-        for (i = 0; i < n; ++i) 
-            scanf("%ld", &arr[i]);
+        for (i = 0; i < n; ++i)
+            scanf("%lld", &arr[i]);
     }
 
-    MPI_Scatter(arr, 1, MPI_LONG, &rec, 1, MPI_LONG, 0, MPI_COMM_WORLD);
-    printf("Process [%d] received = %ld.\n", rank, rec);
+    MPI_Scatter(arr, 1, MPI_LONG_LONG, &rec, 1, MPI_LONG_LONG, 0, MCW);
+    printf("Process [%d] received = %lld.\n", rank, rec);
     fact = 1;
-    for (i = 2; i <= rec; ++i) 
+    for (i = 2; i <= rec; ++i)
         fact *= i;
 
-    MPI_Gather(&fact, 1, MPI_LONG, facts, 1, MPI_LONG, 0, MPI_COMM_WORLD);
-    
+    MPI_Gather(&fact, 1, MPI_LONG_LONG, facts, 1, MPI_LONG_LONG, 0, MCW);
+
     if (rank == 0) {
         printf("Sum of factorials = ");
-        long sum = 0;
-        for (i = 0; i < n; ++i){
+        ll int sum = 0;
+        for (i = 0; i < n; ++i) {
             sum += facts[i];
-            printf("%ld %s", facts[i], (i != n-1)?"+ ":" ");
+            printf("%lld %s", facts[i], (i != n - 1) ? "+ " : " ");
         }
-        printf(" = %ld\n", sum);
+        printf(" = %lld\n", sum);
     }
+
     MPI_Finalize();
+    return 0;
 }
