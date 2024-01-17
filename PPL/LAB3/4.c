ae2b284709331d827bfccf34ffa1fa28d7b2a295
KushalaAravapalli <96609641+Kushala01@users.noreply.github.com>
2024-01-17 16:11:14 +0530

    C

--- PPL/LAB3/4.c
+++ PPL/LAB3/4.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <string.h>
+#define MPI_COMM_WORLD MCW
 
 int main(int argc, char* argv []) {
     int rank, size;
@@ -8,8 +9,8 @@ int main(int argc, char* argv []) {
     int i, m;
     MPI_Init(&argc, &argv);
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MCW, &rank);
+    MPI_Comm_size(MCW, &size);
     
     if (rank == 0) { 
         printf("Enter string 1: ");
@@ -19,15 +20,15 @@ int main(int argc, char* argv []) {
         m = strlen(str1) / size;
     }
     
-    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(str1, m, MPI_CHAR, c1, m, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Scatter(str2, m, MPI_CHAR, c2, m, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&m, 1, MPI_INT, 0, MCW);
+    MPI_Scatter(str1, m, MPI_CHAR, c1, m, MPI_CHAR, 0, MCW);
+    MPI_Scatter(str2, m, MPI_CHAR, c2, m, MPI_CHAR, 0, MCW);
     
     for (i = 0; i < m; i++) {
         concatted[2 * i] = c1[i];
         concatted[2 * i + 1] = c2[i];   
     }
-    MPI_Gather(concatted, 2*m, MPI_CHAR, b, 2*m, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Gather(concatted, 2*m, MPI_CHAR, b, 2*m, MPI_CHAR, 0, MCW);
     
     if (rank == 0)
         printf("Concatted: %s\n", b);
