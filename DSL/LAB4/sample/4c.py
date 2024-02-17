886bb06f185b5ef9fe1ca78fc76289b2345c0b1a
Kushala01 <wjsjkushala9@gmail.com>
2024-02-17 16:33:07 +0530

    prac

--- DSL/LAB4/sample/4c.py
+++ DSL/LAB4/sample/4c.py
@@ -1,1 +1,10 @@
 #WAP to create TCP time server in Python
+import socket
+import time
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host=socket.gethostname()
+port=4545
+s.connect((host,port))
+time=s.recv(1024)
+print('time sent fromt he ser ver is ',time)
+s.close()
--- DSL/LAB4/sample/4s.py
+++ DSL/LAB4/sample/4s.py
@@ -1,1 +1,14 @@
 #WAP to create TCP time server in Python
+import socket
+import time
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host=socket.gethostname()
+port=4545
+s.bind((host,port))
+s.listen(5)
+while True:
+    clientSock, addr=s.accept()
+    print('got a connection from: ',addr)
+    currentTime=time.ctime(time.time())
+    clientSock.send(currentTime.encode('ascii'))
+    clientSock.close()
