1f6271dc9c5f54587ee054566af1fc88cb9d11e5
Kushala01 <wjsjkushala9@gmail.com>
2024-02-18 21:22:45 +0530

    prac

--- DSL/LAB1/prac.py
+++ DSL/LAB1/prac.py
@@ -1,38 +1,46 @@
-age=20
-print(age)
-is_online=True
-print(age, is_online)
-#name=input("what is your name? ")
-name=20
-int(name)
-print("hello" , name)
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
 
-a, b, c = 1, 2, "john"
-print(c)
+# Print the second element
+print(my_list[1])
 
-var1 = 'Hello World!'
-print ("Updated String : ", name, 'Python')
-print( "My name is %s and weight is %d kg!" % ('Abay', 55))
-str = "this is string example wow!!!"
-print (str.capitalize())
-print(str)
+# Modify the first element
+my_list[0] = 10
+print(my_list)
 
-str = "this is string example ... wow!!!"
-print (str.swapcase())
-print(str.upper())
-print(str.lower())
-print(str.find('h')+1)
-print(str.replace("this","bitxh"))
-#are changes in python string inmutable?
-print('string' in str)
+# Append an element
+my_list.append(9)
 
+# Count the occurrences of an element (1 in this case)
+count_of_1 = my_list.count(1)
+print("Count of 1:", count_of_1)
 
-print(10+3)
-print(10-3)
-print(10*3)
-print(10/3)
-print(10//3)
-print(10%3)
-print(10**3)#to the power
-x=10
-x+=3
+# Delete an element by index
+del my_list[2]
+print(my_list)
+
+# Remove an element by value if it exists
+if 3 in my_list:
+    my_list.remove(3)
+else:
+    print("3 is not in the list")
+
+# Insert an element at a specific index
+my_list.insert(3, "what")
+print(my_list)
+
+# Reverse the list
+my_list.reverse()
+print(my_list)
+
+# Concatenate the list with another list
+sub_list = my_list + [10, 11, 12]
+print(sub_list)
+
+# Slice the list to get a sublist
+sub_list = my_list[2:]
+print(sub_list)
+
+#tuple with even numbers
+my_tuple=(1,2,3,4,5,6,7,8,9)
+new_tup=tuple(i for i in my_tuple if i %2==0)
+print(new_tup)
--- DSL/LAB4/Lab Exercise/q1s.py
+++ DSL/LAB4/Lab Exercise/q1s.py
@@ -6,8 +6,8 @@ host = '127.0.0.1'
 port = 6996
 s.bind((host,port))
 while True:
-    data,addr  =s.recvfrom(1024)
+    data, addr=s.recvfrom(1024)
     print('Got Connection from : ',str(addr))
-    Time = time.ctime(time.time())+"\r\n"
+    Time = time.ctime(time.time())
     s.sendto(Time.encode(),addr)
 s.close()
